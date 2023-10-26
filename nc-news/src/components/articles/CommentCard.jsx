6b8745cb1a970e7f709a82623c2aa6c8cca3d939
OzzyCF <o5wa1@hotmail.com>
2023-10-26 10:41:50 +0100

    implement task 11 delete comments

--- nc-news/src/components/articles/CommentCard.jsx
+++ nc-news/src/components/articles/CommentCard.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { timeSince } from "../../utils/timeUtils";
+import { UserContext } from "../../contexts/UserContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-function CommentCard({ comment }) {
-  const daysAgo = Math.ceil(
-    (new Date() - new Date(comment.created_at)) / (1000 * 60 * 60 * 24)
-  );
+function CommentCard({ comment, onDelete }) {
+  const { user } = useContext(UserContext);
+
+  const handleDelete = () => {
+    axios
+      .delete(
+        `https://backend-news-api-ozzycf.onrender.com/api/comments/${comment.comment_id}`
+      )
+      .then(() => {
+        onDelete(comment.comment_id);
+      })
+      .catch((error) => {
+        console.error("Error deleting the comment:", error);
+        toast.error("Failed to delete the comment. Please try again.");
+      });
+  };
 
   return (
     <div className="comment-bubble">
@@ -14,6 +29,11 @@ function CommentCard({ comment }) {
       <p>{comment.body}</p>
       <p>Posted: {timeSince(comment.created_at)}</p>
       <p>Votes: {comment.votes}</p>
+      {user && user.username === comment.author && (
+        <button className="comment-delete" onClick={handleDelete}>
+          Delete
+        </button>
+      )}
     </div>
   );
 }
